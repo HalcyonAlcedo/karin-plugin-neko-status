77645d52e2a895cd86217f0bb96ddba64294d857
zyc404 <zyc404@gmail.com>
2024-06-14 11:00:34 +0800

    修改小组件路径

--- manage/widgets/status.js
+++ manage/widgets/status.js
@@ -3,5 +3,5 @@ import { getMemoryUsageCircle } from '../../lib/components/Memory.js'
 export default async () => {
   const cpu = await getCpuLoadAndSpeed()
   const memory = await getMemoryUsageCircle()
-  return {cpu, memory, col: 2}
+  return {cpu, memory, col: 3}
 }
